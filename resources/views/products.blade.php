1a7a48e6d4ef1c535c4b6443489dc6e0b2f867f2
mahbub095 <mahbub.095@gmail.com>
2023-07-05 16:45:29 +0600

    Products frontend

---
+++ resources/views/products.blade.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet"
+        href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+    <title>Ajax crud</title>
+</head>
+
+<body>
+    <div class="containter">
+        <div class="row">
+            <div class="col-md-2"></div>
+            <div class="col-md-8">
+                <h2 class="my-3">Test Ajax crud </h2>
+                <div class="table-data">
+
+                    <table class="table table-bordered">
+                        <thead class="thead-dark">
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Product</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>
+                                    <a href="">
+                                        <i class="las la-edit"></i>
+                                    </a>
+                                    <a href="">
+                                        <i class="las la-edit"></i>
+                                    </a>
+                                </td>
+                            </tr>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</body>
+
+</html>
